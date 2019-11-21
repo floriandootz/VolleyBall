a3821b7ace6be731d52be96420f6d1ee54349eb1
Florian Dootz <git@floriandootz.de>
2019-11-21 21:11:29 +0100

    Add first example-code

--- app/src/main/java/de/floriandootz/volleyballexample/MainActivity.kt
+++ app/src/main/java/de/floriandootz/volleyballexample/MainActivity.kt
@@ -2,10 +2,39 @@ package de.floriandootz.volleyballexample
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import de.floriandootz.volleyball.parse.Parser
+import de.floriandootz.volleyball.request.Requester
 
-class MainActivity : Activity() {
+class MainActivity : Activity(), Parser<String>, Response.Listener<String>, Response.ErrorListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val requester = Requester(this, true, null, null)
+        requester.request(
+            "https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap",
+            this,
+            true,
+            this,
+            this
+        )
     }
+
+    override fun parse(jsonString: String, headers: Map<String, String>?): String {
+        return jsonString
+    }
+
+    override fun onResponse(response: String) {
+        findViewById<TextView>(R.id.text).text = response
+    }
+
+    override fun onErrorResponse(error: VolleyError?) {
+
+    }
+
 }
